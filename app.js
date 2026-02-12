a81f7b27102101051af51ebd172e68f928dbc736
Katarzyna Poloczek <57349608+timon216@users.noreply.github.com>
2026-02-12 16:13:17 +0100

    add default data and ui elements' variables

---
+++ app.js
@@ -0,0 +1,17 @@
+// default data
+
+const defaultSubjects = [
+  { id: "s1", name: "WDI – ćw." },
+  { id: "s2", name: "WDI – wykład" },
+  { id: "s3", name: "Poetyka – ćw." }
+];
+
+const defaultTasks = [
+  { id: "t1", title: "Zadanie 1", subjectId: "s1", dueDate: "2026-02-12" },
+  { id: "t2", title: "Esej", subjectId: "s3", dueDate: "2026-02-15" }
+];
+
+const defaultEvents = [
+  { id: "e1", title: "Kartkówka", subjectId: "s1", date: "2026-02-13" },
+  { id: "e2", title: "Kolokwium", subjectId: "s2", date: "2026-02-16" }
+];
